f5037655d0bfeea66c539fc8ab8c4246d2b61694
MaxOvcharov <ovcharovmax@yandex.ru>
2017-03-20 22:51:48 +0300

    Added example python code which use xml.etree lib

---
+++ xml_ElementTree/xml_ElementTree_test.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from xml.etree import ElementTree as ET
+
+tcusers = ET.parse('tomcat-users.xml')
+print tcusers
+
+first_user = tcusers.find('./user')
+print first_user
+
+print first_user.attrib
+print first_user.get('name')
+print first_user.tag
+print first_user.text
+
+for user in [e for e in tcusers.findall('./user') if e.get('name') == 'tomcat']:
+    print user.attrib
+
+
