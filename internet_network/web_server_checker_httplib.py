e7dcd2813dc9c6f3c4cf692a3a1d11d8a41762ff
MaxOvcharov <ovcharovmax@yandex.ru>
2017-04-09 18:45:43 +0300

    Added HTTP web-server checke with the help of httplib

--- internet_network/web_server_checker_httplib.py
+++ internet_network/web_server_checker_httplib.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import re
-import socket
+import httplib
 import sys
 
 
@@ -16,42 +15,23 @@ def check_webserver(address, port, resource):
     # Create http resource line
     if not resource.startswith('/'):
         resource = '/' + resource
-    request_string = 'GET %s HTTP/1.1\r\nHost: %s\r\n\r\n' % (resource, address)
-    print 'HTTP request:'
-    print '|||%s|||' % request_string
-
-    # Create socket connection
-    s = socket.socket()
-    print 'Attempting to connect to %s on port %s' % (address, port)
     try:
-        s.connect((address, port))
-        print 'Connected to %s on port %s' % (address, port)
-        s.send(request_string)
-        # Get first 100 bytes
-        rsp = s.recv(100)
-        print 'Received 100 bytes of HTTP responce'
-        print '|||%s|||' % rsp
-    except socket.error, e:
-        print 'Connection to %s on port %s failed: %s' % (address, port, e)
+        conn = httplib.HTTPConnection(address, port)
+        print 'HTTP connection created successfully'
+        # Sent request
+        req = conn.request('GET', resource)
+        print 'Request for %s successfull' % resource
+        # Get response
+        response = conn.getresponse()
+        print 'Response status %s' % response.status
+    except httplib.error, e:
+        print 'HTTP connection failed: %s' % e
         return False
     finally:
-        # Close socket connection
-        print 'Closing the connection...'
-        s.close()
-
-    lines = rsp.splitlines()
-    print 'First line of HTTP responce: %s' % lines[0]
-    try:
-        version, status, message = re.split(r'\s+', lines[0], 2)
-        print 'Version: %s, status: %s, massage: %s' % (version, status, message)
-    except ValueError:
-        print 'Failed to splite status line'
-        return False
-    if status in ['200', '301']:
-        print 'Success - status was %s' % status
+        print 'HTTP connection closed successfully'
+    if response.status in [200, 301]:
         return True
     else:
-        print 'Status was %s' % status
         return False
 
 if __name__ == '__main__':
