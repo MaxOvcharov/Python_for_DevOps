a0c122ac7455a98addcf57dd10408e0222cd6d6f
MaxOvcharov <ovcharovmax@yandex.ru>
2017-04-17 15:55:29 +0300

    Added Duplicate file finder with recursive walk thru the path

---
+++ sys_admin_utils_for_working_with_data/duplicate_file_finder.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+
+from checksum_checker import create_checksum
+from disk_path_scanner import RecursivePathWalker
+from os.path import getsize
+
+
+def find_duplicates(path='/tmp'):
+    """
+    Recursive finder of all duplicate path
+    :param path: paht name
+    :return:
+    """
+    dup = []
+    record = {}
+    d = RecursivePathWalker(path)
+    files = d.enumerate_path()
+    for file in files:
+        compound_key = (getsize(file), create_checksum(file))
+        if compound_key in record:
+            dup.append(file)
+        else:
+            record[compound_key] = file
+    return dup
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print "Find duplicates for default dir -> /tmp"
+        duplicates = find_duplicates()
+    else:
+        duplicates = find_duplicates(sys.argv[1])
+    print '//-//-' * 9 + '\n'
+    for dup in enumerate(duplicates, start=1):
+        print "%s) Duplicate file: %s" % dup
