c5c7edb9fe9f5fbffe33f61b414d4e29fe2a4187
MaxOvcharov <ovcharovmax@yandex.ru>
2017-04-24 22:26:30 +0300

    Added simple script for SNMP connetion

---
+++ snmp_utils/snmp.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import netsnmp
+
+
+class Snmp(object):
+    """Simple SNMP connection """
+
+    def __init__(self,
+                 oid='sysDescr',
+                 version=2,
+                 dest_host='localhost',
+                 community='public'):
+
+        self.oid = oid
+        self.version = version
+        self.dest_host = dest_host
+        self.community = community
+        
+    def query(self):
+        """Simple SNMP query"""
+        try:
+            result = netsnmp.snmwalk(self.oid,
+                                     Version=self.version,
+                                     DestHost=self.dest_host,
+                                     Community=self.community)
+        except Exception, e:
+            print "HANDLE ERROR: %s" % e
+            result = None
+        return result
