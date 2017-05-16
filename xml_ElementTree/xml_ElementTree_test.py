94ed8df924863e4013cad07ec158ca8ab17c0da8
MaxOvcharov <ovcharovmax@yandex.ru>
2017-05-16 23:02:10 +0300

    Modify xml_ElementTree to python3

--- xml_ElementTree/xml_ElementTree_test.py
+++ xml_ElementTree/xml_ElementTree_test.py
@@ -3,17 +3,17 @@
 from xml.etree import ElementTree as ET
 
 tcusers = ET.parse('tomcat-users.xml')
-print tcusers
+print(tcusers)
 
 first_user = tcusers.find('./user')
-print first_user
+print(first_user)
 
-print first_user.attrib
-print first_user.get('name')
-print first_user.tag
-print first_user.text
+print(first_user.attrib)
+print(first_user.get('name'))
+print(first_user.tag)
+print(first_user.text)
 
 for user in [e for e in tcusers.findall('./user') if e.get('name') == 'tomcat']:
-    print user.attrib
+    print(user.attrib)
 
 
